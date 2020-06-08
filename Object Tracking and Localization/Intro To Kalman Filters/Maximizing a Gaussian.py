992ba0e30443034bd3234c2f0d9477b3682a3bec
Zain <zainullahdev@outlook.com>
2020-06-08 21:17:17 +0400

    feat: maximizing gaussian

---
+++ Object Tracking and Localization/Intro To Kalman Filters/Maximizing a Gaussian.py
@@ -0,0 +1,9 @@
+from math import *
+
+def f(mu, sigma2, x):
+    return 1 / sqrt(2 * pi * sigma2) * exp(-0.5 * (x - mu)**2 / sigma2)
+
+print(f(10,4,8))
+
+# maximum return value of function f
+print('maximum value', f(10,4,10))
